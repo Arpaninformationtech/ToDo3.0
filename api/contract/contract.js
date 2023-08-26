293ff6b6bc8b46a4323c213b21c59c8502d296ff
ArpanMandal <actuallyarpan@gmail.com>
2023-08-27 01:01:59 +0530

    new1

--- api/contract/contract.js
+++ api/contract/contract.js
@@ -1,6 +1,6 @@
 const {Web3}= require("web3");
 const ABI = require("../ABI.json");
-const web3 = new Web3("https://eth-sepolia.g.alchemy.com/v2/74mlskD0sX4li0gK-ljIuG7LRVW135la")
+const web3 = new Web3("https://eth-sepolia.g.alchemy.com/v2/lHL9kU7v2P9ADqRtFFfGHWJEm7PCCfLY")
 const contractAddress = "0x1d0a7A032e32cEd928131Dcf4856455A36ED7454";
 const contract = new web3.eth.Contract(ABI,contractAddress);
 
